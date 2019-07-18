754b94da2a84b40e664a36f1cb1de9de57c6aa51
Denys Zhorov <2013dvl@gmail.com>
2019-07-18 11:39:47 +0200

    intial prodject and connect api

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import { async } from "q";
+
+export default class BookService {
+    constructor(){
+
+    const __apiBase = "http://reactjs-hsnet.mocklab.io/book";
+
+        async function getResource() {
+            const res = await fetch(`${__apiBase}`);
+            if (!res.ok) {
+                throw new Error(`Could not fetch ${__apiBase}` +
+                    ` received ${res.status}`);
+            }
+            return await res.json();
+        }
+
+    this.getAllBook = async () => {
+        const res = await getResource();
+        return res.books.map(this._transformBook);
+    }
+    this._transformBook = (book) => {
+        return{
+           id:  book.bookId,
+           title: book.bookName,
+           author: book.author,
+           price: book.price,
+           coverImage: book.img,
+           pagesAmount:  book.pagesAmount
+        }
+    }
+}   
+}
