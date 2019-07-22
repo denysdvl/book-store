d6c0add675c8501a17b1ae5acdd1f257cdefbcc6
Denys Zhorov <2013dvl@gmail.com>
2019-07-22 15:23:06 +0200

    Updates

---
+++ precache-manifest.a6841cd3ad32933364b6df5680c5991e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d919c700b9fa07d58b6f8d44eb4848be",
+    "url": "/book-store/index.html"
+  },
+  {
+    "revision": "1cdaa3b8e30e1554b046",
+    "url": "/book-store/static/css/main.016b1c3d.chunk.css"
+  },
+  {
+    "revision": "ec08a76997baaedc0554",
+    "url": "/book-store/static/js/2.7a69abd7.chunk.js"
+  },
+  {
+    "revision": "1cdaa3b8e30e1554b046",
+    "url": "/book-store/static/js/main.7dffe014.chunk.js"
+  },
+  {
+    "revision": "53d1302e424e81137a5b",
+    "url": "/book-store/static/js/runtime~main.a6495b40.js"
+  }
+]);
